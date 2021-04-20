171f81ffcb1b0f78c075a2c51b0b94092d9524db
jbp <17600558224@163.com>
2021-04-20 10:57:39 +0800

    这是一次测试提交

---
+++ cashloan-manage/src/main/java/com/ajaya/cashloan/manage/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.ajaya.cashloan.manage.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+    public String Test(){
+        return "这是一个测试控制器";
+    }
+}
